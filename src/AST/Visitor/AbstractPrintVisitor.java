5a9c0f5eb10aa5932a4f6327222390144c144d46
Jaechan Lee <chanylee77@gmail.com>
2024-10-22 16:38:27 -0700

    drastic updates to minijava.cup

---
+++ src/AST/Visitor/AbstractPrintVisitor.java
@@ -0,0 +1,421 @@
+package AST.Visitor;
+
+import AST.*;
+
+public class AbstractPrintVisitor implements Visitor {
+    
+    private int indentation;
+    private static final String TAB = "\t";
+
+    public AbstractPrintVisitor() {
+        super();
+        indentation = 0;
+    }
+
+    private void print(String message) {
+        System.out.print(message);
+    }
+
+    private void println() {
+        print("\n");
+    }
+
+    private void printsp() {
+        print(" ");
+    }
+
+    private void printIndent() {
+        for (int i = 0; i < indentation; ++i) {
+            print(TAB);
+        }
+    }
+
+    private void printIndent(String message) {
+        printIndent();
+        print(message);
+    }
+
+    private void printlnIndent(String message) {
+        printIndent(message);
+        println();
+    }
+
+    private void printLineNumber(ASTNode n) {
+        print(" (line " + n.line_number + ")");
+    }
+
+    // MainClass m;
+    // ClassDeclList cl;
+    @Override
+    public void visit(Program n) {
+        printlnIndent("Program");
+        indentation++;
+        n.m.accept(this);
+        for (int i = 0; i < n.cl.size(); i++) {
+            println();
+            printIndent();
+            n.cl.get(i).accept(this);
+        }
+        indentation--;
+    }
+
+    // Identifier i1,i2;
+    // Statement s;
+    @Override
+    public void visit(MainClass n) {
+        printIndent("MainClass ");
+        n.i1.accept(this);
+        printLineNumber(n);
+        println();
+        indentation++;
+        n.s.accept(this);
+        indentation--;
+    }
+
+    // Identifier i;
+    // VarDeclList vl;
+    // MethodDeclList ml;
+    @Override
+    public void visit(ClassDeclSimple n) {
+        printIndent("Class ");
+        n.i.accept(this);
+        printLineNumber(n);
+        println();
+
+        indentation++;
+        for (int i = 0; i < n.vl.size(); i++) {
+            n.vl.get(i).accept(this);
+            println();
+        }
+        for (int i = 0; i < n.ml.size(); i++) {
+            n.ml.get(i).accept(this);
+            if (i != n.ml.size() - 1) {
+                println();
+            }
+        }
+        indentation--;
+    }
+
+    // Identifier i;
+    // VarDeclList vl;
+    // MethodDeclList ml;
+    @Override
+    public void visit(ClassDeclExtends n) {
+        printIndent("Class ");
+        n.i.accept(this);
+        printIndent(" extends ");
+        n.j.accept(this);
+        printLineNumber(n);
+        println();
+
+        indentation++;
+        for (int i = 0; i < n.vl.size(); i++) {
+            n.vl.get(i).accept(this);
+            println();
+        }
+        for (int i = 0; i < n.ml.size(); i++) {
+            n.ml.get(i).accept(this);
+            if (i != n.ml.size() - 1) {
+                println();
+            }
+        }
+        indentation--;
+    }
+
+    // Type t;
+    // Identifier i;
+    @Override
+    public void visit(VarDecl n) {
+        printlnIndent("VarDecl ");
+        n.t.accept(this);
+        printsp();
+        n.i.accept(this);
+        printLineNumber(n);
+    }
+
+    // Type t;
+    // Identifier i;
+    // FormalList fl;
+    // VarDeclList vl;
+    // StatementList sl;
+    // Exp e;
+    @Override
+    public void visit(MethodDecl n) {
+        printlnIndent("MethodDecl ");
+        n.i.accept(this);   // e.g., bar
+        printLineNumber(n);
+        indentation++;
+        printIndent("returns ");
+        n.t.accept(this);   // e.g., int
+        printlnIndent("parameters:");
+        for (int i = 0; i < n.fl.size(); i++) {
+            indentation++;
+            n.fl.get(i).accept(this);
+            indentation--;
+        }
+        for (int i = 0; i < n.sl.size(); i++) {
+            n.sl.get(i).accept(this);
+        }
+        if (n.e != null) {
+            printIndent("Return ");
+            n.e.accept(this);
+            printLineNumber(n);
+            println();
+        }
+        indentation--;
+    }
+
+    // Type t;
+    // Identifier i;
+    @Override
+    public void visit(Formal n) {
+        n.t.accept(this);
+        System.out.print(" ");
+        n.i.accept(this);
+    }
+
+    @Override
+    public void visit(IntArrayType n) {
+        print("int []");
+    }
+
+    @Override
+    public void visit(BooleanType n) {
+        print("boolean");
+    }
+
+    @Override
+    public void visit(IntegerType n) {
+        print("int");
+    }
+
+    // String s;
+    @Override
+    public void visit(IdentifierType n) {
+        print(n.s);
+    }
+
+    // StatementList sl;
+    @Override
+    public void visit(Block n) {
+        printIndent("Block ");
+        printLineNumber(n);
+        indentation++;
+        for (int i = 0; i < n.sl.size(); i++) {
+            n.sl.get(i).accept(this);
+        }
+        indentation--;
+    }
+
+    // Exp e;
+    // Statement s1,s2;
+    @Override
+    public void visit(If n) {
+        printIndent("If ");
+        printLineNumber(n);
+        println();
+        indentation++;
+        printIndent("condition: ");
+        n.e.accept(this);
+        println();
+        n.s1.accept(this);
+        indentation--;
+        if (n.s2 != null) {
+            printlnIndent("Else");
+            indentation++;
+            n.s2.accept(this);
+            indentation--;
+        }
+    }
+
+    // Exp e;
+    // Statement s;
+    @Override
+    public void visit(While n) {
+        printlnIndent("While ");
+        printLineNumber(n);
+        println();
+        indentation++;
+        printIndent("condition: ");
+        n.e.accept(this);
+        println();
+        n.s.accept(this);
+        indentation--;
+    }
+
+    // Exp e;
+    @Override
+    public void visit(Print n) {
+        printIndent("Print ");
+        n.e.accept(this);
+        printLineNumber(n);
+        println();
+    }
+
+    // Identifier i;
+    // Exp e;
+    @Override
+    public void visit(Assign n) {
+        printIndent("Assign ");
+        n.i.accept(this);
+        print(" = ");
+        n.e.accept(this);
+        printLineNumber(n);
+        println();
+    }
+
+    // Identifier i;
+    // Exp e1,e2;
+    @Override
+    public void visit(ArrayAssign n) {
+        printIndent("ArrayAssign ");
+        n.i.accept(this);
+        print("[");
+        n.e1.accept(this);
+        print("] = ");
+        n.e2.accept(this);
+        printLineNumber(n);
+        println();
+    }
+
+    // Exp e1,e2;
+    @Override
+    public void visit(And n) {
+        print("(");
+        n.e1.accept(this);
+        print(" && ");
+        n.e2.accept(this);
+        print(")");
+    }
+
+    // Exp e1,e2;
+    @Override
+    public void visit(LessThan n) {
+        print("(");
+        n.e1.accept(this);
+        print(" < ");
+        n.e2.accept(this);
+        print(")");
+    }
+
+    // Exp e1,e2;
+    @Override
+    public void visit(Plus n) {
+        print("(");
+        n.e1.accept(this);
+        print(" + ");
+        n.e2.accept(this);
+        print(")");
+    }
+
+    // Exp e1,e2;
+    @Override
+    public void visit(Minus n) {
+        print("(");
+        n.e1.accept(this);
+        print(" - ");
+        n.e2.accept(this);
+        print(")");
+    }
+
+    // Exp e1,e2;
+    @Override
+    public void visit(Times n) {
+        print("(");
+        n.e1.accept(this);
+        print(" * ");
+        n.e2.accept(this);
+        print(")");
+    }
+
+    // Exp e1,e2;
+    @Override
+    public void visit(ArrayLookup n) {
+        n.e1.accept(this);
+        print("[");
+        n.e2.accept(this);
+        print("]");
+    }
+
+    // Exp e;
+    @Override
+    public void visit(ArrayLength n) {
+        n.e.accept(this);
+        print(".length");
+    }
+
+    // Exp e;
+    // Identifier i;
+    // ExpList el;
+    @Override
+    public void visit(Call n) {
+        n.e.accept(this);
+        print(".");
+        n.i.accept(this);
+        print("(");
+        for (int i = 0; i < n.el.size(); i++) {
+            n.el.get(i).accept(this);
+            if (i + 1 < n.el.size()) {
+                print(", ");
+            }
+        }
+        print(")");
+    }
+
+    // int i;
+    @Override
+    public void visit(IntegerLiteral n) {
+        print(String.valueOf(n.i));
+    }
+
+    @Override
+    public void visit(True n) {
+        print("true");
+    }
+
+    @Override
+    public void visit(False n) {
+        print("false");
+    }
+
+    // String s;
+    @Override
+    public void visit(IdentifierExp n) {
+        print(n.s);
+    }
+
+    @Override
+    public void visit(This n) {
+        print("this");
+    }
+
+    // Exp e;
+    @Override
+    public void visit(NewArray n) {
+        print("new int[");
+        n.e.accept(this);
+        print("]");
+    }
+
+    // Identifier i;
+    @Override
+    public void visit(NewObject n) {
+        print("new ");
+        n.i.accept(this);
+        print("()");
+    }
+
+    // Exp e;
+    @Override
+    public void visit(Not n) {
+        print("!");
+        n.e.accept(this);
+    }
+
+    // String s;
+    @Override
+    public void visit(Identifier n) {
+        print(n.s);
+    }
+
+}
