6ee084c7d77a8b5cc74b63e9f94f4f058d70176b
Vladimirtrif <59485365+Vladimirtrif@users.noreply.github.com>
2024-11-26 15:08:43 -0800

    Fixed typechecking so println so accepts booleans. Wrote basic test for compiler.

--- SamplePrograms/MySampleMiniJavaPrograms/TestPrint.java
+++ SamplePrograms/MySampleMiniJavaPrograms/TestPrint.java
@@ -1,5 +1,22 @@
 class TestPrint {
     public static void main(String[] a){
-        System.out.println(20);
+        System.out.println(new BasicTests().runTests());
     }
+}
+
+class BasicTests {
+
+    public int runTests() {
+
+        System.out.println(10);
+        System.out.println(11 + 9);
+        System.out.println(40 - 10);
+        System.out.println(25 * 2);
+        System.out.println(true);
+        System.out.println(true && false);
+        System.out.println(20 < 21);
+        System.out.println(!true);
+        return 0;
+    }
+
 }
